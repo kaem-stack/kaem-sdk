ed789a281e47e12df1a9ede2c8265d8bbe856647
Mohammad Zbib <mohammad.h.zbib@gmail.com>
2026-05-17 20:17:48 +0300

    chore(transport): set it up as kaem official sdk for binaries

--- src/transport.rs
+++ src/transport.rs
@@ -1,29 +1,56 @@
+use bytes::{Buf, BufMut, BytesMut};
 use serde::{Serialize, de::DeserializeOwned};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::marker::PhantomData;
 use tokio::net::UnixStream;
+use tokio_util::codec::{Decoder, Encoder, Framed};
 
-pub async fn send_message<T: Serialize>(
-    stream: &mut UnixStream,
-    msg: &T,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let bytes = bincode::serialize(msg)?;
-    let len = bytes.len() as u32;
-
-    stream.write_all(&len.to_be_bytes()).await?;
-    stream.write_all(&bytes).await?;
-
-    Ok(())
+pub struct IpcCodec<T> {
+    _phantom: PhantomData<T>,
 }
 
-pub async fn receive_message<T: DeserializeOwned>(
-    stream: &mut UnixStream,
-) -> Result<T, Box<dyn std::error::Error>> {
-    let mut len_buf = [0u8; 4];
-    stream.read_exact(&mut len_buf).await?;
-    let len = u32::from_be_bytes(len_buf) as usize;
+impl<T> IpcCodec<T> {
+    pub fn new() -> Self {
+        Self { _phantom: PhantomData }
+    }
+}
 
-    let mut buf = vec![0u8; len];
-    stream.read_exact(&mut buf).await?;
+impl<T> Default for IpcCodec<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
-    Ok(bincode::deserialize(&buf)?)
+impl<T: Serialize> Encoder<T> for IpcCodec<T> {
+    type Error = Box<dyn std::error::Error + Send + Sync>;
+
+    fn encode(&mut self, item: T, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        let bytes = bincode::serialize(&item)?;
+        dst.reserve(4 + bytes.len());
+        dst.put_u32(bytes.len() as u32);
+        dst.extend_from_slice(&bytes);
+        Ok(())
+    }
+}
+
+impl<T: DeserializeOwned> Decoder for IpcCodec<T> {
+    type Item = T;
+    type Error = Box<dyn std::error::Error + Send + Sync>;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<T>, Self::Error> {
+        if src.len() < 4 {
+            return Ok(None);
+        }
+        let len = u32::from_be_bytes(src[..4].try_into().unwrap()) as usize;
+        if src.len() < 4 + len {
+            src.reserve(4 + len - src.len());
+            return Ok(None);
+        }
+        src.advance(4);
+        let data = src.split_to(len);
+        Ok(Some(bincode::deserialize(&data)?))
+    }
+}
+
+pub fn framed<T>(stream: UnixStream) -> Framed<UnixStream, IpcCodec<T>> {
+    Framed::new(stream, IpcCodec::new())
 }
